6d089509bc0ccda3baa77925e64902998b4aa522
astrid <k1ndled11@gmail.com>
2022-07-10 00:02:33 -0400

    Return HTML instead
    Allows for more styling, and most importantly, meta tags!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Router = require('@tsndr/cloudflare-worker-router')
-const {createHash} = require("crypto");
+const { createHash } = require('crypto')
 const router = new Router()
 
 router.cors()
@@ -14,7 +14,37 @@ router.get('/', (req, res) => {
 
     // Get the IP from the request
     const ip = req.headers.get('CF-Connecting-IP')
-    res.body = createHash('sha256').update(ip).digest('hex')
+
+    res.headers.set('content-type', 'text/html;charset=UTF-8')
+
+    const desc = `Generate an IP hash instantly`
+
+    res.body = `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hasher</title>
+        <meta property="og:title" content="Hasher" />
+        <meta property="og:description" content="${desc}" />
+        <meta name="description" content="${desc}" />
+        <meta name="theme-color" content="#ad6edd">
+    </head>
+    <body>
+    <style>
+        body {
+            background-color: black;
+        }
+        h3 {
+            color: white;
+            margin-left: 0.5rem;
+        }
+    </style>
+        <h3 class="hehe">  ${createHash('sha256')
+            .update(ip)
+            .digest('hex')}</h3>
+    </body>
+</html>
+`
 })
 
 addEventListener('fetch', event => {
